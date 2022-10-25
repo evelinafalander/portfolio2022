f2c3e8ff554bec2e850c9692581538a56ebfeed7
evelinafalander <evel0838@stud.kea.dk>
2022-10-25 16:27:17 +0200

    fixes

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
-const items = document.querySelectorAll("#horisontalliste li");
-scroll(
-  animate("#horisontalliste", {
-    transform: ["none", `translateX(-${items.length - 1}00vw)`],
-  }),
-  { target: document.querySelector("#horisontalscrollsection") }
-);
+//  SCROLL IN OPACITY ANIMATION
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
